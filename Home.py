5e72841820f65b3457b1a34f565eac6625af215d
SohanMahadev1 <142841320+SohanMahadev1@users.noreply.github.com>
2024-04-14 22:48:46 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,18 +1,1 @@
-import streamlit as st
-from Hospital import show_hospital_page
-from app2 import show_crime_page
 
-def show_home():
-    st.title("Welcome to the Safety & Health Locator App")
-    st.write("Please select one of the options below to continue:")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.button("Hospital Finder"):
-            show_hospital_page()
-    with col2:
-        if st.button("Crime Spot Locator"):
-            show_crime_page()
-
-if __name__ == "__main__":
-    show_home()
